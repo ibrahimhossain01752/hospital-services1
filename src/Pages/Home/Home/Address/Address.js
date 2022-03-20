d4a5ae8af50d2bcef65e81c1bc50b1ecfe778da9
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-03-20 15:41:37 +0600

    address added

---
+++ src/Pages/Home/Home/Address/Address.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Address.css';
+import {  BsFillAlarmFill} from "react-icons/bs";
+
+const Address = () => {
+    return (
+        <div class="container address22">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class=" h-100">
+     
+      <div class="card-body">
+      <BsFillAlarmFill class="icons1"></BsFillAlarmFill><h5 class="card-title "> <span class="location1">Location</span>  <span class="location">Boshundhara, Dhaka 1212 Bangladesh</span> </h5>
+       
+       
+      </div>
+    
+    </div>
+  </div>
+  <div class="col">
+    <div class=" h-100">
+     
+      <div class="card-body address23">
+      <BsFillAlarmFill class="icons1"></BsFillAlarmFill><h5 class="card-title "> <span class="location1">Location</span>  <span class="location">Boshundhara, Dhaka 1212 Bangladesh</span> </h5>
+       
+       
+      </div>
+    
+    </div>
+  </div>
+  <div class="col">
+    <div class=" h-100">
+     
+      <div class="card-body">
+      <BsFillAlarmFill class="icons1"></BsFillAlarmFill><h5 class="card-title "> <span class="location1">Location</span>  <span class="location">Boshundhara, Dhaka 1212 Bangladesh</span> </h5>
+       
+       
+      </div>
+    
+    </div>
+  </div>
+  
+  
+</div>
+        </div>
+    );
+};
+
+export default Address;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Address from './Address/Address';
 import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Address></Address>
         </div>
     );
 };
