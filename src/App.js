1fde7d18e82738db19acc9134ad607c25c9bb41a
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-03-20 11:54:04 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+     <BrowserRouter>
+     <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+       
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
